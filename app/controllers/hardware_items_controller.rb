66f7902b0535b3b118d3cabaa7073ad97e73aef0
Grant Ammons <gammons@gmail.com>
2014-02-13 07:27:17 -0500

    remove turbolinks, add views

--- app/controllers/hardware_items_controller.rb
+++ app/controllers/hardware_items_controller.rb
@@ -2,14 +2,4 @@ class HardwareItemsController < ApplicationController
   def index
     @items = HardwareItem.all
   end
-
-  def create
-
-  end
-
-
-  def user_params
-    params.require(:hardware_item).permit(:manufacturer, :hardware_model, :link, :instructions_link, :description, :cost, :algorithm, :speed_gh_s, :power_w, :is_available, :availability_comment, :hardware_type)
-    end
-  end
 end
