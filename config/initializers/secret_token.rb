b413ef9ea5ec7ac18ae1c3c676f7f0b726001f92
Grant Ammons <gammons@gmail.com>
2014-02-13 07:08:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BitcoinHardwareComparison::Application.config.secret_key_base = 'fdf6eb520e5ac86d231132bf58552f6b2c9d53dcfbcd9255c0d187bc0e3ecaa1be3ff00acc2d0fc4b42430e3818a8757d01b3c8bd0b04da915cc12bfd7cc6ac9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BitcoinHardwareComparison::Application.config.session_store :cookie_store, key: '_bitcoin-hardware-comparison_session'
